f16ff3cbabec8d044d4c9a175cfd88e889716633
robsavoye <robsavoye@users.noreply.github.com>
2021-04-10 16:06:53 +0000

    deploy: 0300cdc84136ed68654e2947c255df3217b8c0a5

--- search/functions_1.js
+++ search/functions_1.js
@@ -3,6 +3,6 @@ var searchData=
   ['checkpoi_230',['checkPOI',['../classvalidate_1_1Validate.html#a2683db7a66ccb2f68e11baabb0223c3a',1,'validate::Validate']]],
   ['checkway_231',['checkWay',['../classvalidate_1_1Validate.html#ad2ca5055e5b1c69901a2ccba31163f12',1,'validate::Validate']]],
   ['collectstats_232',['collectStats',['../classosmchange_1_1OsmChangeFile.html#a4a6035b16ec815be6e0289b65bcbaaad',1,'osmchange::OsmChangeFile']]],
-  ['connect_233',['connect',['../classosmstats_1_1QueryOSMStats.html#ab9a82ad73f64b9837d40a2950166ac3e',1,'osmstats::QueryOSMStats::connect()'],['../classgeoutil_1_1GeoUtil.html#a5b77bbd1bbd8af82e6d14a8055274b95',1,'geoutil::GeoUtil::connect()'],['../classimport_1_1OSMHandler.html#ac7c09a13bbcb8a5d3dbdbb94c12b67ff',1,'import::OSMHandler::connect()'],['../classunderpass_1_1Underpass.html#a2efc7cced45c818f42f30925e46940ec',1,'underpass::Underpass::connect()']]],
+  ['connect_233',['connect',['../classosmstats_1_1QueryOSMStats.html#a11f9103e3f03983062a5deb4a711ddc9',1,'osmstats::QueryOSMStats::connect()'],['../classgeoutil_1_1GeoUtil.html#a5b77bbd1bbd8af82e6d14a8055274b95',1,'geoutil::GeoUtil::connect()'],['../classimport_1_1OSMHandler.html#ac7c09a13bbcb8a5d3dbdbb94c12b67ff',1,'import::OSMHandler::connect()'],['../classunderpass_1_1Underpass.html#afae2b2d84cfca9729406005b24f7dd0d',1,'underpass::Underpass::connect()']]],
   ['currentnode_234',['currentNode',['../classosmchange_1_1OsmChange.html#a27e3e7f0e8505f7cf80cb0d320737ca5',1,'osmchange::OsmChange']]]
 ];
