478a7944755187f199e5f9bb4e29e5a783ca9119
robsavoye <robsavoye@users.noreply.github.com>
2021-05-29 14:49:59 +0000

    deploy: 04bcbd470b2990b8c4bf368c2a664168547a0b90

--- search/functions_0.js
+++ search/functions_0.js
@@ -5,7 +5,7 @@ var searchData=
   ['addcountry_213',['addCountry',['../classosmstats_1_1QueryOSMStats.html#a98b9be0afffdc12662092e0b8282dd89',1,'osmstats::QueryOSMStats']]],
   ['addeditor_214',['addEditor',['../classchangeset_1_1ChangeSet.html#aa59b2a0d812437dfcc24b617ec5a00d7',1,'changeset::ChangeSet']]],
   ['addhashtag_215',['addHashtag',['../classosmstats_1_1QueryOSMStats.html#a53eed0c1e352516e1c40a794de121192',1,'osmstats::QueryOSMStats']]],
-  ['addhashtags_216',['addHashtags',['../classchangeset_1_1ChangeSet.html#a317c9e7e51d909c07aeeaa32803676b7',1,'changeset::ChangeSet']]],
+  ['addhashtags_216',['addHashtags',['../classchangeset_1_1ChangeSet.html#ac9de3adefa7c5b92be4b512f63b0baf0',1,'changeset::ChangeSet']]],
   ['addref_217',['addRef',['../classosmchange_1_1OsmChange.html#a17eed3f5cffef24191c3f21b352e5005',1,'osmchange::OsmChange::addRef()'],['../classosmobjects_1_1OsmWay.html#a7c08aba02b4b4ca50f9daabcb618a99b',1,'osmobjects::OsmWay::addRef()']]],
   ['addtag_218',['addTag',['../classosmchange_1_1OsmChange.html#ae31047767d68437ad3bec8de0f7fd655',1,'osmchange::OsmChange::addTag()'],['../classosmobjects_1_1OsmObject.html#aff542cf48d9489c6ebcb0ffe339c5346',1,'osmobjects::OsmObject::addTag()']]],
   ['adduser_219',['addUser',['../classosmstats_1_1QueryOSMStats.html#a44fb42216a8c90b6dcafec62ee97f6dc',1,'osmstats::QueryOSMStats']]],
